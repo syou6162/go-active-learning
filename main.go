634e53e4db7263df712156b7c71bfa4b812a8824
Yasuhisa Yoshida <syou6162@gmail.com>
2017-05-13 22:32:00 +0900

    ユーザーから標準入力経由でアノテーションできるように

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/mattn/go-tty"
+)
+
+func ask(e *Example) (bool, error) {
+	fmt.Println("Label this example: " + e.url)
+	t, err := tty.Open()
+	defer t.Close()
+	if err != nil {
+		return false, err
+	}
+	var r rune
+	for r == 0 {
+		r, err = t.ReadRune()
+		if err != nil {
+			return false, err
+		}
+	}
+	switch r {
+	case 'p':
+		e.Annotate(POSITIVE)
+		fmt.Println("Labeled as positive")
+		return true, nil
+	case 'n':
+		e.Annotate(NEGATIVE)
+		fmt.Println("Labeled as negative")
+		return true, nil
+	default:
+		return false, nil
+	}
+}
+
+func main() {
+	examples, _ := ReadExamples(os.Args[1])
+
+	for {
+		e := RandomSelectOneExample(examples)
+		if e == nil {
+			break
+		}
+		ask(e)
+	}
+
+	for _, e := range examples {
+		fmt.Println(e)
+	}
 }
