b8b7ac73eb2350f2c6e42df08144e9e094bd6674
Yasuhisa Yoshida <syou6162@gmail.com>
2017-05-13 23:59:02 +0900

    名詞のみBoWで返す関数を追加

--- feature.go
+++ feature.go
@@ -1,3 +1,19 @@
 package main
 
+import (
+	"github.com/ikawaha/kagome/tokenizer"
+)
+
 type FeatureVector []string
+
+func ExtractFeatures(title string) FeatureVector {
+	var fv FeatureVector
+	t := tokenizer.New()
+	tokens := t.Tokenize(title)
+	for _, token := range tokens {
+		if token.Pos() == "名詞" {
+			fv = append(fv, token.Surface)
+		}
+	}
+	return fv
+}
