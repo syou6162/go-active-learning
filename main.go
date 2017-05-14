6fdd5162995c601cdc612a71503135305fcd3844
Yasuhisa Yoshida <syou6162@gmail.com>
2017-05-14 12:58:48 +0900

    平均化パーセプトロンで学習回す

--- main.go
+++ main.go
@@ -64,6 +64,20 @@ func main() {
 		e.fv = ExtractFeatures(e.title)
 	}
 
+	train := FilterLabeledExamples(examples)
+
+	model := NewModel()
+	trainGolds := ExtractGoldLabels(train)
+	for iter := 0; iter < 10; iter++ {
+		for _, example := range train {
+			model.Learn(*example)
+		}
+
+		trainPredicts := make([]LabelType, len(train))
+		for i, example := range train {
+			trainPredicts[i] = model.Predict(example.fv)
+		}
+		fmt.Println(fmt.Sprintf("Iter:%d\tAccuracy:%0.03f", iter, GetAccuracy(trainGolds, trainPredicts)))
 	}
 
 annotationLoop:
