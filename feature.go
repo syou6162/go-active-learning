f37d0efede00cef11db029dc280240fc37d17cf6
Yasuhisa Yoshida <syou6162@gmail.com>
2017-05-15 15:21:46 +0900

    bodyやdescriptionからも特徴量を取る

--- feature.go
+++ feature.go
@@ -6,14 +6,22 @@ import (
 
 type FeatureVector []string
 
-func ExtractFeatures(title string) FeatureVector {
+func ExtractNounFeatures(s string, prefix string) FeatureVector {
 	var fv FeatureVector
 	t := tokenizer.New()
-	tokens := t.Tokenize(title)
+	tokens := t.Tokenize(s)
 	for _, token := range tokens {
 		if token.Pos() == "名詞" {
-			fv = append(fv, token.Surface)
+			fv = append(fv, prefix + ":" + token.Surface)
 		}
 	}
+	return fv
+}
+
+func ExtractFeatures(e Example) FeatureVector {
+	var fv FeatureVector
+	fv = append(fv, ExtractNounFeatures(e.Title, "TITLE")...)
+	fv = append(fv, ExtractNounFeatures(e.Description, "DESCRIPTION")...)
+	fv = append(fv, ExtractNounFeatures(e.Body, "BODY")...)
 	return fv
 }
