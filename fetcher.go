d38456c25437ad183a0f33b8ed7c0f005a1cc510
Yasuhisa Yoshida <syou6162@gmail.com>
2017-05-15 16:57:25 +0900

    GoOseがタイムアウトしてくれるので、自前ではやらない

--- fetcher.go
+++ fetcher.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"time"
 	"github.com/advancedlogic/GoOse"
 )
 
@@ -12,22 +11,11 @@ type Article struct {
 	Body        string
 }
 
-func getArticle(url string) Article {
+func GetArticle(url string) Article {
 	g := goose.New()
 	article, err := g.ExtractFromURL(url)
 	if err != nil {
 		return Article{}
 	}
 	return Article{url, article.Title, article.MetaDescription, article.CleanedText}
-}
-
-func GetArticle(url string) Article {
-	ch := make(chan Article, 1)
-	go func(url string) { ch <- getArticle(url) }(url)
-	select {
-	case article := <-ch:
-		return article
-	case <-time.After(10 * time.Second):
-		return Article{}
-	}
 }
