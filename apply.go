19da454ccb77c679a99c38db1aeda355df413762
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-06 12:58:34 +0900

    足切りの閾値を設定

--- apply.go
+++ apply.go
@@ -18,6 +18,7 @@ func doApply(c *cli.Context) error {
 	sizeConstraint := c.Int("size-constraint")
 	alpha := c.Float64("alpha")
 	r := c.Float64("r")
+	scoreThreshold := c.Float64("score-threshold")
 
 	if inputFilename == "" {
 		_ = cli.ShowCommandHelp(c, "apply")
@@ -46,7 +47,7 @@ func doApply(c *cli.Context) error {
 	for _, e := range FilterUnlabeledExamples(examples) {
 		e.Score = model.PredictScore(e.Fv)
 		e.Title = strings.Replace(e.Title, "\n", " ", -1)
-		if e.Score > 0.0 {
+		if e.Score > scoreThreshold {
 			result = append(result, e)
 		}
 	}
@@ -88,5 +89,6 @@ Apply classifier to unlabeled examples, and print a pair of score and url.
 		cli.Int64Flag{Name: "size-constraint", Value: 10, Usage: "Budget constraint. Max number of entries to be contained"},
 		cli.Float64Flag{Name: "alpha", Value: 1.0},
 		cli.Float64Flag{Name: "r", Value: 1.0, Usage: "Scaling factor for number of words"},
+		cli.Float64Flag{Name: "score-threshold", Value: 0.0},
 	},
 }
