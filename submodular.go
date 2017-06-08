438dd32ac5b7021e9508cfe429335a14b7f2aedb
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-09 08:47:43 +0900

    類似度はTF-IDFを使ったほうがよさそう

--- submodular.go
+++ submodular.go
@@ -7,7 +7,8 @@ import (
 func SelectSubExamplesBySubModular(model BinaryClassifier, whole Examples, sizeConstraint int, alpha float64, r float64) Examples {
 	selected := Examples{}
 	remainings := whole
-	simMat := GetSimilarityMatrixByFeatureWeight(model, whole)
+	simMat := GetSimilarityMatrixByTFIDF(whole)
+	// simMat := GetSimilarityMatrixByFeatureWeight(model, whole)
 	for {
 		if len(selected) >= sizeConstraint || len(remainings) == 0 {
 			break
@@ -72,6 +73,41 @@ func GetSimilarityMatrixByFeatureWeight(model BinaryClassifier, examples Example
 	return mat
 }
 
+func GetSimilarityMatrixByTFIDF(examples Examples) SimilarityMatrix {
+	idf := GetIDF(examples)
+
+	dfByURL := make(map[string]map[string]float64)
+	sumByUrl := make(map[string]float64)
+	for _, e := range examples {
+		df := GetDF(*e)
+		dfByURL[e.Url] = df
+
+		sum := 0.0
+		for k, v := range df {
+			sum += v * v * idf[k] * idf[k]
+		}
+		sumByUrl[e.Url] = sum
+	}
+
+	mat := SimilarityMatrix{}
+	for _, e1 := range examples {
+		df1 := dfByURL[e1.Url]
+		s1 := math.Sqrt(sumByUrl[e1.Url])
+
+		for _, e2 := range examples {
+			df2 := dfByURL[e2.Url]
+			s2 := math.Sqrt(sumByUrl[e1.Url])
+
+			s := 0.0
+			for k, v := range df2 {
+				s += v * df1[k] * idf[k] * idf[k]
+			}
+			mat[e1.Url+"+"+e2.Url] = s / (s1 * s2)
+		}
+	}
+	return mat
+}
+
 func GetCosineSimilarity(mat SimilarityMatrix, e1 *Example, e2 *Example) float64 {
 	return mat[e1.Url+"+"+e2.Url]
 }
@@ -105,3 +141,38 @@ func Norm(model BinaryClassifier, e *Example) float64 {
 	}
 	return math.Sqrt(sum)
 }
+
+func GetDF(example Example) map[string]float64 {
+	df := make(map[string]float64)
+	n := 0.0
+	fv := ExtractNounFeatures(example.Body, "BODY")
+
+	for _, f := range fv {
+		df[f]++
+		n++
+	}
+
+	for k, v := range df {
+		df[k] = v/ n
+	}
+	return df
+}
+
+func GetIDF(examples Examples) map[string]float64 {
+	idf := make(map[string]float64)
+	cnt := make(map[string]float64)
+	n := float64(len(examples))
+
+	for _, e := range examples {
+		fv := ExtractNounFeatures(e.Body, "BODY")
+		for _, f := range fv {
+			cnt[f]++
+		}
+	}
+
+	for k, v := range cnt {
+		idf[k] = math.Log(n / v) + 1
+	}
+	return idf
+}
+
