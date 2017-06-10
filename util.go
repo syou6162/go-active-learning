c23441a0f983c902eed97e137020e6e2807edecd
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-10 23:40:10 +0900

    境界条件間違っていた

--- util.go
+++ util.go
@@ -196,5 +196,5 @@ func NewOutputFilename() string {
 func splitTrainAndDev(examples Examples) (train Examples, dev Examples) {
 	shuffle(examples)
 	n := int(0.8 * float64(len(examples)))
-	return examples[0:n], examples[n+1 : len(examples)-1]
+	return examples[0:n], examples[n:]
 }
