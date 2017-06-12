30104f4cace4b072769c11dd5328c1af41fa2ff7
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-12 09:51:34 +0900

    SortByScoreのテストを追加

---
+++ binary_classifier_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSortByScore(t *testing.T) {
+	e1 := NewExample("http://b.hatena.ne.jp", POSITIVE)
+	e1.Title = "bookmark"; e1.Fv = []string{"hoge", "fuga"}
+	e2 := NewExample("http://google.com", NEGATIVE)
+	e2.Title = "google"; e2.Fv = []string{"piyo", "aaa"}
+	e3 := NewExample("http://hatena.ne.jp", UNLABELED)
+	e3.Title = "hatena"; e3.Fv = []string{"hoge", "fuga"}
+	e4 := NewExample("http://hogehoge.com", UNLABELED)
+	e4.Title = "hogehoge"; e4.Fv = []string{"piyo", "hoge"}
+
+	examples := Examples{e1, e2, e3, e4}
+	c := NewBinaryClassifier(examples)
+	examples = SortByScore(c, examples)
+
+	if len(examples) != 2 {
+		t.Errorf("len(example) == %d, want 2", len(examples))
+	}
+
+	if examples[0].Title != "hogehoge" {
+		t.Errorf("example[0].Title == %s, want 'hogehoge'", examples[0].Title)
+	}
+}
