c73b41f1130631c1b688842a4bc64e2a8231605a
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-14 20:46:22 +0900

    SelectSubExamplesBySubModularのテストを追加

--- submodular_test.go
+++ submodular_test.go
@@ -26,3 +26,16 @@ func TestGetIDF(t *testing.T) {
 		t.Error(fmt.Printf("Example must contain %s", japan))
 	}
 }
+
+func TestSelectSubExamplesBySubModular(t *testing.T) {
+	e1 := NewExample("http://b.hatena.ne.jp", POSITIVE)
+	e1.Body = "こんにちは、日本"
+	e2 := NewExample("http://google.com", POSITIVE)
+	e2.Body = "hello google"
+
+	examples := SelectSubExamplesBySubModular(Examples{e1, e2}, 1, 1.0, 1.0)
+
+	if len(examples) != 1 {
+		t.Error(fmt.Printf("Number of selected examples must be %d", len(examples)))
+	}
+}
