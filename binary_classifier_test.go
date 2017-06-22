472795d15848677ee2da847c7d0af2bf26534665
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-23 00:24:37 +0900

    特徴量と事例の調整

--- binary_classifier_test.go
+++ binary_classifier_test.go
@@ -5,9 +5,12 @@ import (
 )
 
 func TestSortByScore(t *testing.T) {
+	e0 := NewExample("http://www.yasuhisay.info/", POSITIVE)
+	e0.Title = "yasuhisa"
+	e0.Fv = []string{"hoge", "fuga"}
 	e1 := NewExample("http://b.hatena.ne.jp", POSITIVE)
 	e1.Title = "bookmark"
-	e1.Fv = []string{"hoge", "fuga"}
+	e1.Fv = []string{"hoge", "fuga", "aaa"}
 	e2 := NewExample("http://google.com", NEGATIVE)
 	e2.Title = "google"
 	e2.Fv = []string{"piyo", "aaa"}
@@ -18,7 +21,7 @@ func TestSortByScore(t *testing.T) {
 	e4.Title = "hogehoge"
 	e4.Fv = []string{"piyo", "hoge"}
 
-	examples := Examples{e1, e2, e3, e4}
+	examples := Examples{e0, e1, e2, e3, e4}
 	c := NewBinaryClassifier(examples)
 	examples = SortByScore(c, examples)
 
