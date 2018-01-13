6e9b8e6cf3471976dd5e28e23269a68877506644
Yasuhisa Yoshida <syou6162@gmail.com>
2018-01-13 16:25:02 +0900

    loadとsaveはredisでは必要ない

--- cache_test.go
+++ cache_test.go
@@ -22,36 +22,3 @@ func TestCacheGet(t *testing.T) {
 		t.Error(fmt.Printf("Urls must be same(%s, %s)", example.Url, e.Url))
 	}
 }
-
-func TestCacheSave(t *testing.T) {
-	c := NewCache()
-	example := NewExample("http://b.hatena.ne.jp", POSITIVE)
-	c.Add(*example)
-	c.Get(*example)
-	err := c.Save(CacheFilename)
-
-	if err != nil {
-		t.Error(fmt.Printf("Error (%s) occurs when saving cache file", err))
-	}
-}
-
-func TestLoadCache(t *testing.T) {
-	c := NewCache()
-	example := NewExample("http://b.hatena.ne.jp", POSITIVE)
-	c.Add(*example)
-	c.Get(*example)
-	c.Save(CacheFilename)
-
-	c, err := LoadCache(CacheFilename)
-	if err != nil {
-		t.Error(fmt.Printf("Error (%s) occurs when loading cache file", err))
-	}
-
-	e, ok := c.Get(*example)
-	if !ok {
-		t.Error(fmt.Printf("Cache must return %s", example.Url))
-	}
-	if example.Url != e.Url {
-		t.Error(fmt.Printf("Urls must be same(%s, %s)", example.Url, e.Url))
-	}
-}
