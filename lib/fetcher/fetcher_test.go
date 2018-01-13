02daaeae28ab18b444b8fdddd9aeca91c658f6e9
Yasuhisa Yoshida <syou6162@gmail.com>
2018-01-14 03:22:16 +0900

    remove print

--- lib/fetcher/fetcher_test.go
+++ lib/fetcher/fetcher_test.go
@@ -1,13 +1,11 @@
 package fetcher
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetArticle(t *testing.T) {
 	a := GetArticle("http://www.yasuhisay.info/entry/20090516/1242480413")
-	fmt.Println(a)
 
 	if a.Title == "" {
 		t.Error("Title must not be empty")
