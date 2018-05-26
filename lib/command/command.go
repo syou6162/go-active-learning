8b112604067cfb03f0d5c18a392c663973e8a9b2
Yasuhisa Yoshida <syou6162@gmail.com>
2018-05-26 23:03:14 +0900

    active learning以外はリポジトリを移す

--- lib/command/command.go
+++ lib/command/command.go
@@ -3,12 +3,10 @@ package command
 import (
 	"github.com/codegangsta/cli"
 	"github.com/syou6162/go-active-learning/lib/annotation"
-	"github.com/syou6162/go-active-learning/lib/apply"
 	"github.com/syou6162/go-active-learning/lib/diagnosis"
 )
 
 var Commands = []cli.Command{
 	annotation.CommandAnnotate,
-	apply.CommandApply,
 	diagnosis.CommandDiagnose,
 }
