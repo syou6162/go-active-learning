aa5e00af575c6e7c301ef55bbb3ffafeb3b81d0c
Yasuhisa Yoshida <syou6162@gmail.com>
2018-05-26 17:22:20 +0900

    expand-urlとwebは別リポジトリに分ける

--- lib/command/command.go
+++ lib/command/command.go
@@ -5,14 +5,10 @@ import (
 	"github.com/syou6162/go-active-learning/lib/annotation"
 	"github.com/syou6162/go-active-learning/lib/apply"
 	"github.com/syou6162/go-active-learning/lib/diagnosis"
-	"github.com/syou6162/go-active-learning/lib/expand_url"
-	"github.com/syou6162/go-active-learning/lib/web"
 )
 
 var Commands = []cli.Command{
 	annotation.CommandAnnotate,
 	apply.CommandApply,
-	expand_url.CommandExpandURL,
 	diagnosis.CommandDiagnose,
-	web.CommandServe,
 }
