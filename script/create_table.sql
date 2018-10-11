229f17febac5f022f0ab6a2b6c9b122c6b6fddb5
Yasuhisa Yoshida <syou6162@gmail.com>
2018-10-11 19:24:36 +0900

    インデックスを貼ります

--- script/create_table.sql
+++ script/create_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS example (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS "url_idx_example" ON example ("url");
+CREATE INDEX IF NOT EXISTS "label_updated_at_idx_example" ON example ("label", "updated_at" DESC);
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO nobody;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO nobody;
