589a90e89fd6594b6d90b7d12aba72797ed7f63e
Yasuhisa Yoshida <syou6162@gmail.com>
2018-10-14 17:31:26 +0900

    テスト追加

--- lib/fetcher/fetcher_test.go
+++ lib/fetcher/fetcher_test.go
@@ -13,6 +13,9 @@ func TestGetArticle(t *testing.T) {
 	if a.Description == "" {
 		t.Error("Description must not be empty")
 	}
+	if a.OgType != "article" {
+		t.Error("OgType must be article")
+	}
 	if a.StatusCode != 200 {
 		t.Error("StatusCode must be 200")
 	}
