6d0419602a964c28ac96f52aecdd53ba84647379
Yasuhisa Yoshida <syou6162@gmail.com>
2018-10-25 19:51:33 +0900

    lightバージョンのテストも追加

--- lib/cache/cache_test.go
+++ lib/cache/cache_test.go
@@ -76,6 +76,56 @@ func TestAttachMetaData(t *testing.T) {
 		t.Errorf("Title must be empty for %s", examples[0].Url)
 	}
 	if len(examples[0].Fv) == 0 {
+		t.Errorf("Feature vector must not be empty for %s", examples[0].Url)
+	}
+
+	if examples[1].OgType != "blog" {
+		t.Errorf("OgType must be blog for %s", examples[1].Url)
+	}
+}
+
+func TestAttachLightMetaData(t *testing.T) {
+	e1 := example.NewExample("http://b.hatena.ne.jp", example.POSITIVE)
+	e2 := example.NewExample("http://www.yasuhisay.info", example.NEGATIVE)
+	e3 := example.NewExample("https://github.com", example.UNLABELED)
+	examples := example.Examples{e1, e2, e3}
+	for _, e := range examples {
+		key := "url:" + e.Url
+		client.Del(key)
+	}
+
+	if examples[0].Title != "" {
+		t.Errorf("Title must be empty for %s", examples[0].Url)
+	}
+	if len(examples[0].Fv) != 0 {
+		t.Errorf("Feature vector must be empty for %s", examples[0].Url)
+	}
+	AttachMetadata(examples, false, false)
+
+	if examples[0].Title != "" {
+		t.Errorf("Title must be empty for %s", examples[0].Url)
+	}
+	if len(examples[0].Fv) != 0 {
+		t.Errorf("Feature vector must be empty for %s", examples[0].Url)
+	}
+
+	if examples[1].OgType != "" {
+		t.Errorf("OgType must be empty for %s", examples[1].Url)
+	}
+
+	AttachMetadata(examples, true, true)
+
+	e1 = example.NewExample("http://b.hatena.ne.jp", example.POSITIVE)
+	e2 = example.NewExample("http://www.yasuhisay.info", example.NEGATIVE)
+	e3 = example.NewExample("https://github.com", example.UNLABELED)
+	examples = example.Examples{e1, e2, e3}
+
+	AttachMetadata(examples, false, true)
+
+	if examples[0].Title == "" {
+		t.Errorf("Title must not be empty for %s", examples[0].Url)
+	}
+	if len(examples[0].Fv) != 0 {
 		t.Errorf("Feature vector must be empty for %s", examples[0].Url)
 	}
 
