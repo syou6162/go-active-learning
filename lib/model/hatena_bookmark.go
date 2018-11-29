85ab46c345cee5daa2f7d95566441b1276a12493
Yasuhisa Yoshida <syou6162@gmail.com>
2018-11-29 23:58:04 +0900

    テーブル構造検討

--- lib/model/hatena_bookmark.go
+++ lib/model/hatena_bookmark.go
@@ -1,25 +1,45 @@
 package model
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 type Tags []string
 
+type HatenaBookmarkTime struct {
+	*time.Time
+}
+
+// ref: https://dev.classmethod.jp/go/struct-json/
+func (hbt *HatenaBookmarkTime) UnmarshalJSON(data []byte) error {
+	t, err := time.Parse("\"2006/01/02 15:04\"", string(data))
+	*hbt = HatenaBookmarkTime{&t}
+	return err
+}
+
+func (hbt HatenaBookmarkTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hbt.Format("2006/01/02 15:04"))
+}
+
 type Bookmark struct {
-	// Timestamp time.Time `json:"timestamp"`
-	// 使わないし、unmarshalが面倒なのでひとまずなしで
-	User    string `json:"user"`
-	Tags    Tags   `json:"tags"`
-	Comment string `json:"comment"`
+	HatenaBookmarkId int                `db:"hatena_bookmark_id"`
+	Timestamp        HatenaBookmarkTime `json:"timestamp" db:"timestamp"`
+	User             string             `json:"user" db:"user"`
+	Tags             Tags               `json:"tags" db:"tags"`
+	Comment          string             `json:"comment" db:"comment"`
 }
 
 type HatenaBookmark struct {
-	Title      string      `json:"title"`
+	Id         int         `db:"id"`
+	ExampleId  int         `db:"example_id"`
+	Title      string      `json:"title" db:"title"`
 	Bookmarks  []*Bookmark `json:"bookmarks"`
-	Screenshot string      `json:"screenshot"`
-	EntryUrl   string      `json:"entry_url"`
-	Count      int         `json:"count"`
-	Url        string      `json:"url"`
-	EId        string      `json:"eid"`
+	Screenshot string      `json:"screenshot" db:"screenshot"`
+	EntryUrl   string      `json:"entry_url" db:"entry_url"`
+	Count      int         `json:"count" db:"count"`
+	Url        string      `json:"url" db:"url"`
+	EId        string      `json:"eid" db:"eid"`
 }
 
 func (bookmarks *HatenaBookmark) MarshalBinary() ([]byte, error) {
