40bd8b73aafbb5d94d2d115360735cb39cbbb75c
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-02 05:20:08 +0900

    fmt

--- lib/service/example_test.go
+++ lib/service/example_test.go
@@ -2,6 +2,7 @@ package service_test
 
 import (
 	"testing"
+
 	"github.com/syou6162/go-active-learning/lib/example"
 	"github.com/syou6162/go-active-learning/lib/model"
 	"github.com/syou6162/go-active-learning/lib/service"
