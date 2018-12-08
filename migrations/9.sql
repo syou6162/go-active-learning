451088dc4df29bbf1b2fdd4d481fd588c3b27cc3
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-08 14:59:58 +0900

    sql追加し忘れていた

---
+++ migrations/9.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS model (
+  "model" TEXT NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS "created_at_model" ON example ("created_at");
+
+-- +migrate Down
+DROP INDEX "created_at_model";
+
+DROP TABLE model;
