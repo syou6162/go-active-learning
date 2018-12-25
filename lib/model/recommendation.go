fb85a921a6533059bae01a1d970540cdfdbe4884
Yasuhisa Yoshida <syou6162@gmail.com>
2018-12-25 17:31:35 +0900

    idとlistnameの変換ができるように

--- lib/model/recommendation.go
+++ lib/model/recommendation.go
@@ -1,5 +1,7 @@
 package model
 
+import "fmt"
+
 type RecommendationListType int
 
 const (
@@ -9,6 +11,23 @@ const (
 	SLIDE   RecommendationListType = 3
 	ARXIV   RecommendationListType = 4
 )
+
+func GetRecommendationListType(listname string) (RecommendationListType, error) {
+	switch listname {
+	case "general":
+		return GENERAL, nil
+	case "article":
+		return ARTICLE, nil
+	case "github":
+		return GITHUB, nil
+	case "slide":
+		return SLIDE, nil
+	case "arxiv":
+		return ARXIV, nil
+	default:
+		return -1, fmt.Errorf("no such RecommendationListType for '%s'", listname)
+	}
+}
 
 type Recommendation struct {
 	RecommendationListType RecommendationListType
