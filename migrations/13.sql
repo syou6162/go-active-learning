43fc64e0aff131b4c119cb3c26be899d1fe1c96d
Yasuhisa Yoshida <syou6162@gmail.com>
2019-01-20 13:19:11 +0900

    初めにインデックス消さないとdownできない

--- migrations/13.sql
+++ migrations/13.sql
@@ -10,6 +10,8 @@ DROP INDEX "created_at_model";
 CREATE INDEX IF NOT EXISTS "model_type_created_at_model" ON model ("model_type", "created_at");
 
 -- +migrate Down
+DROP INDEX "model_type_created_at_model";
+
 ALTER TABLE "model" DROP COLUMN "model_type";
 ALTER TABLE "model" DROP COLUMN "c";
 ALTER TABLE "model" DROP COLUMN "accuracy";
@@ -18,4 +20,3 @@ ALTER TABLE "model" DROP COLUMN "recall";
 ALTER TABLE "model" DROP COLUMN "fvalue";
 
 CREATE INDEX IF NOT EXISTS "created_at_model" ON model ("created_at");
-DROP INDEX "model_type_created_at_model";
