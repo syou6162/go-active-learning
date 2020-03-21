df75ff4b5b8fa0d3f6e555a9e7da8b73b367ca04
Yasuhisa Yoshida <syou6162@gmail.com>
2020-03-21 19:38:21 +0900

    制約のテスト

--- lib/repository/related_example_test.go
+++ lib/repository/related_example_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/syou6162/go-active-learning/lib/repository"
 )
 
-func TestUpdateUpdateRelatedExamples(t *testing.T) {
+func TestUpdateRelatedExamples(t *testing.T) {
 	repo, err := repository.New()
 	if err != nil {
 		t.Errorf(err.Error())
@@ -39,5 +39,29 @@ func TestUpdateUpdateRelatedExamples(t *testing.T) {
 		if len(related.RelatedExampleIds) != 2 {
 			t.Error("len(related.RelatedExampleIds) must be 2")
 		}
+	}
+}
+
+func TestUpdateRelatedExamplesMyOwn(t *testing.T) {
+	repo, err := repository.New()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer repo.Close()
+
+	e1 := example.NewExample("http://hoge1.com", model.POSITIVE)
+	e2 := example.NewExample("http://hoge2.com", model.NEGATIVE)
+	e3 := example.NewExample("http://hoge3.com", model.UNLABELED)
+	examples := model.Examples{e1, e2, e3}
+	for _, e := range examples {
+		err = repo.UpdateOrCreateExample(e)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	related := model.RelatedExamples{ExampleId: e1.Id, RelatedExampleIds: []int{e1.Id, e2.Id, e3.Id}}
+	err = repo.UpdateRelatedExamples(related)
+	if err == nil {
+		t.Error("自身と同一のexample_idを持つ事例はrelated_example_idに追加できない")
 	}
 }
